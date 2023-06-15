97ed20b06cc607797b5eb2ee96568e024a01db8d
suptimusfika <48064134+suptimusfika@users.noreply.github.com>
2023-06-16 01:55:17 +0600

    Update Problem20_ExtractPrice.java

--- src/main/java/Problem20_ExtractPrice.java
+++ src/main/java/Problem20_ExtractPrice.java
@@ -5,15 +5,18 @@ public class Problem20_ExtractPrice {
     public static void main(String[] args) {
         String paragraph = "A core i 7 laptop price is 85000 tk and a gaming mouse price is 2500 tk. " +
                 "If I buy the laptop and 1 piece mouse, what will be my total cost after giving 15% discount?";
-        String word[] = paragraph.split(" ");
-        String l = word[7];
+        paragraph = paragraph.replaceAll("[^\\d]", " ");
+        paragraph = paragraph.trim();
+        paragraph = paragraph.replaceAll(" +", " ");
+        String[] word=(paragraph.split(" "));
+        String l = word[1];
         int laptopPrice = Integer.parseInt(l);
-        String m = word[15];
+        String m = word[2];
         int mousePrice = Integer.parseInt(m);
-        String numMouse = word[23];
+        String numMouse = word[3];
         int numberOfMouse = Integer.parseInt(numMouse);
-        String dis[] = word[34].split("%");
-        int discount = Integer.parseInt(dis[0]);
+        String dis = word[4];
+        int discount = Integer.parseInt(dis);
         int totalCost = laptopPrice + (numberOfMouse * mousePrice);
         double totalCostWithDiscount = totalCost - (discount * totalCost) / 100;
         System.out.println("Total cost after giving 15% discount = " + totalCostWithDiscount);
